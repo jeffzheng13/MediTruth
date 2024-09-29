532834c787531a36dbc20ef9728363342a1dac3e
Jeffrey Zheng <jeffreyzheng12@gmail.com>
2024-09-28 22:37:58 -0400

    chunking documents and prevent duplicates

--- connectMongo.py
+++ connectMongo.py
@@ -1,4 +1,4 @@
-from typing import List
+from typing import List, Tuple
 from dotenv import load_dotenv
 import os
 from uuid import uuid4
@@ -8,6 +8,7 @@ from pymongo import MongoClient
 
 from langchain_google_genai import GoogleGenerativeAIEmbeddings
 from langchain_core.documents import Document
+from langchain_text_splitters import RecursiveCharacterTextSplitter
 # for when we aren't using gen ai embeddings
 # from langchain_core.embeddings import FakeEmbeddings
 # embeddings = FakeEmbeddings(size=4096)
@@ -27,11 +28,36 @@ class MongoWrapper():
         index_name=atlas_vector_search_index_name,
     )
 
-    def add_to_vector_store(self, documents: List[Document]):
+    #splits documents returned from PubMedAPIWrapper
+    def _text_split(self, paper):
+        text_splitter = RecursiveCharacterTextSplitter(chunk_size = 2000, chunk_overlap = 200, length_function=len)
+        return text_splitter.split_documents(paper)
+
+    #checks if document already exists
+    def _check_doc_exists(self, doc: Document) -> bool:
+        if not doc.metadata: 
+            return False
+        
+        docTitle = doc.metadata["Title"]
+        if self.MONGODB_COLLECTION.find_one({"Title": docTitle}):
+            return True
+
+        return False
+
+    def add_to_vector_store(self, documents: List[Document]) -> None:
+        # using document title as unique identifier
+        unadded_docs = []
+        for doc in documents:
+            if self._check_doc_exists(doc):
+                continue
+            else:
+                unadded_docs.append(doc)
+                
+        documents = self._text_split(unadded_docs)
         uuids = [str(uuid4()) for _ in range(len(documents))]
         self.vector_store.add_documents(documents=documents, ids=uuids)
 
-    def retrieve_vector_store(self, query):
+    def retrieve_vector_store(self, query) -> List[Tuple[Document, float]]:
         # retriever = self.vector_store.as_retriever(
         #     search_type="similarity_score_threshold",
         #     search_kwargs={"k":1, "score_threshold":0.3}
@@ -39,8 +65,10 @@ class MongoWrapper():
         
         # return retriever.invoke(query)
         results = self.vector_store.similarity_search_with_score(query, k=3)
-        for res, score in results:
-            print(f"* [SIM={score:3f}] [{res}]")
+        # filtering by 0.5
+        results = [results for _, score in results if score >= 0.5]
+
+        return results
 
 
 
--- main.py
+++ main.py
@@ -19,15 +19,13 @@ wrapper2.sleep_time = 0.5
 mongo_conn = MongoWrapper()
 for info in facts[0:1]:
     terms = info["search_terms"]
-    #uncomment to add more docs to database
-    # try: 
-    #     docIterator = wrapper.load_docs(terms)
-    #     time.sleep(2)
-    #     docIterator2 = wrapper2.load_docs(terms)
-    #finally:
-        #mongo_conn.add_to_vector_store(docIterator)
-        #mongo_conn.add_to_vector_store(docIterator2)
+    try: 
+        docIterator = wrapper.load_docs(terms)
+        time.sleep(2)
+        docIterator2 = wrapper2.load_docs(terms)
+    finally:
+        mongo_conn.add_to_vector_store(docIterator)
+        mongo_conn.add_to_vector_store(docIterator2)
     print(terms)
     print(info["fact"])
-    print(mongo_conn.retrieve_vector_store(info["fact"]))
     
