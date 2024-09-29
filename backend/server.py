b86538511f8613ba29eef46b45676bd7d2e65c7b
Jeffrey Zheng <jeffreyzheng12@gmail.com>
2024-09-29 02:17:18 -0400

    clean

--- backend/server.py
+++ backend/server.py
@@ -6,31 +6,28 @@ from yt_to_facts import get_health_facts_from_yt_url
 from pubmed import CustomPubMedAPIWrapper
 from connectMongo import MongoWrapper
 from factCheck import fact_check
-
+import re
 
 load_dotenv()
 app = Flask(__name__)
 CORS(app)  # Enable CORS for all routes
 gemini_key = os.getenv("GEMINI_API_KEY")
 
-
-@app.route('/get_facts', methods=['GET'])
-def get_facts():
-    # /get_facts?url=https://youtube.com/id
-    url = request.args.get('url')
-    if not url:
-        return jsonify({"error": "URL parameter is required"}), 400
-
-    data = get_health_facts_from_yt_url(url, gemini_key)
-
-    return jsonify(data)
+@app.route('/health_check', methods=["GET"])
+def health_check():
+    return jsonify({"msg": "API running"}), 200
 
 @app.route('/check_facts', methods=['GET'])
+# /check_facts?url=https://youtube.com/id
 def check_facts():
-    # /check_facts?url=https://youtube.com/id
     url = request.args.get('url')
+    yt_regex = re.compile(r'^.*(youtu.be\/|v\/|u\/\w\/|embed\/|watch\?v=|\&v=|\?v=)([^#\&\?]*).*')
     if not url:
-        return jsonify({"error": "URL parameter is required"}), 400
+        return jsonify({"error": "No YouTube link provided"}), 400
+
+    if not yt_regex.match(url):
+        return jsonify({"error": "Invalid YouTube link"}), 400
+
 
     facts = get_health_facts_from_yt_url(url, gemini_key)
 
